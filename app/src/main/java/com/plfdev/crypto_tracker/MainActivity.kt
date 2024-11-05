1a3b1327a9decd59c432a4d51bf2d44c48af02a6
Pedro L Freitas <pedrollaraf@gmail.com>
2024-11-04 21:41:20 -0300

    add splash screen

--- app/src/main/java/com/plfdev/crypto_tracker/MainActivity.kt
+++ app/src/main/java/com/plfdev/crypto_tracker/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.plfdev.crypto_tracker.core.navigation.AdaptiveCoinsDetailPane
 import com.plfdev.crypto_tracker.ui.theme.CryptoTrackerTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !viewModel.isReady.value
+            }
+        }
         enableEdgeToEdge()
         setContent {
             CryptoTrackerTheme {
